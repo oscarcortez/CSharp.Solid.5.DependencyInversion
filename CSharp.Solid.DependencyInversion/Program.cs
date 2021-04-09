9990abb1b82aff1e50fb814edc897d9b10923601
oscarcortez <oscarkortez@gmail.com>
2021-04-08 20:21:02 -0400

    Add project files.

---
+++ CSharp.Solid.DependencyInversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Solid.DependencyInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
